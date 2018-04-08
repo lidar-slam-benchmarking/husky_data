fe91643dea08ce0a060e30deec3a4888ccd827c3
David Robinson <dvsrobinson@gmail.com>
2018-04-08 19:27:52 -0400

    beginning to nomalize odom and vive

--- src/ground_truth.py
+++ src/ground_truth.py
@@ -3,14 +3,16 @@
 import rospy
 import math
 import tf
+from tf.transformations import euler_from_quaternion
 import numpy as np
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
 from std_msgs.msg import Int16, Int16MultiArray
 from visualization_msgs.msg import Marker
+from nav_msgs.msg import Odometry
 
 import pdb
-
 
 def landmarker(x,y,z,size):
     global landmark_id
@@ -21,7 +23,7 @@ def landmarker(x,y,z,size):
     msg.header.stamp = rospy.Time.now()
     msg.ns = "landmark"
     msg.id = landmark_id
-    msg.type = 2  # cube
+    msg.type = 2  # sphere
     msg.action = 0  # add
     msg.pose.position = Point(x,y,z)
     msg.pose.orientation.w = 1
@@ -35,21 +37,53 @@ def landmarker(x,y,z,size):
     landmark_viz_pub.publish(msg)
     landmark_id += 1
 
+
+def odometryCb(msg):
+
+    p = msg.pose.pose.position
+    r = msg.pose.pose.orientation
+
+    br.sendTransform((p.x,p.y,p.z),
+        (r.x,r.y,r.z,r.w),
+        rospy.Time.now(),
+        "/base_link",
+        "/odom")
+
+def viveCb(msg):
+
+    p = msg.pose.pose.position
+    r = msg.pose.pose.orientation
+    # roll, pitch, yaw = tf.transformations.euler_from_quaternion(quat)
+
+    # normalize!
+    br.sendTransform((p.x,0,p.z),
+        # (r.x,r.y,r.z,r.w),
+        tf.transformations.quaternion_from_euler(0,0,0),
+        rospy.Time.now(),
+        "/vive",
+        "/vive_world")
+
+
 def main():
 
     rospy.init_node('ground_truth')
+    global br
+    br = tf.TransformBroadcaster()
+    listener = tf.TransformListener()
 
     landmarks = rospy.get_param('landmarks')
     vive_base_1 = rospy.get_param('vive_base_1')
     vive_base_2 = rospy.get_param('vive_base_2') # also vive_world apparently
     odom_pos = rospy.get_param('odom_pos')
     odom_rot = rospy.get_param('odom_rot')
-
-    br = tf.TransformBroadcaster()
+    velodyne_offset = rospy.get_param('velodyne_offset')
 
     global landmark_viz_pub, landmark_id
     landmark_viz_pub = rospy.Publisher('landmark_viz', Marker, queue_size=100)
     landmark_id = 0
+
+    rospy.Subscriber('/husky_velocity_controller/odom',Odometry,odometryCb)
+    rospy.Subscriber('/vive/LHR_0EB0243A_odom',Odometry,viveCb)
 
     rospy.sleep(0.3)
     for i in range(0,len(landmarks)):
@@ -62,16 +96,7 @@ def main():
     r = rospy.Rate(100)
     while not rospy.is_shutdown():
 
-        try:
-            (trans,rot) = listener.lookupTransform('/world', '/odom', rospy.Time(0))
-            print trans
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            continue
-
-
-
-
-        ### This is basically a hacaky way to paramterize a static tf ###
+        ### This is basically a hacky way to paramterize a static tf ###
 
         br.sendTransform((vive_base_1[0],vive_base_1[1],0),
             tf.transformations.quaternion_from_euler(1.5708,0,0),
@@ -89,13 +114,19 @@ def main():
             tf.transformations.quaternion_from_euler(1.5708,0,1.5708),
             rospy.Time.now(),
             "/vive_world",
-            "/world") # updating static tf
+            "/world")
 
         br.sendTransform((odom_pos[0],odom_pos[1],odom_pos[2]),
             tf.transformations.quaternion_from_euler(odom_rot[0],odom_rot[1],odom_rot[2]),
             rospy.Time.now(),
             "/odom",
-            "/world") # updating static tf
+            "/world")
+
+        br.sendTransform((velodyne_offset[0],velodyne_offset[1],velodyne_offset[2]),
+            tf.transformations.quaternion_from_euler(0,0,0),
+            rospy.Time.now(),
+            "/laser_link",
+            "/base_link")
 
         r.sleep()
 
